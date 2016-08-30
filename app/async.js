a66639b520bdb54a508287f1396914f9af5d9a18
John Nieves <karn09@gmail.com>
2016-08-30 21:58:50 +0000

    async specs pass

--- app/async.js
+++ app/async.js
@@ -2,10 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(function(resolve, reject) {
+      if (value) resolve(value);
+      reject(value);
+    })
   },
 
   manipulateRemoteData: function(url) {
-
-  }
+    // assuming that 'people' and 'name' exists as keys, with $ in global.
+    return $.getJSON(url)
+      .then(function(data) {
+        return data.people.map(function(person) {
+          return person.name;
+        }).sort()
+      })
+  },
+  
 };
--- app/objects.js
+++ app/objects.js
@@ -2,10 +2,12 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
+    // apply obj context to fn
     return fn.apply(obj)
   },
 
   alterObjects: function(constructor, greeting) {
+    // extend constructor prototype with greeting, return constructor.
     constructor.prototype.greeting = greeting;
     return constructor;
   },
@@ -14,6 +16,7 @@ exports.objectsAnswers = {
     var resultArr = [];
     var str = '';
     for (var key in obj) {
+      // check whether obj has key as direct property. 
       if (obj.hasOwnProperty(key)) {
         str = key + ': ' + obj[key];
         resultArr.push(str)
