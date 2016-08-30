12683479ba85df2af6a6de91b4eff936045c0a53
John Nieves <karn09@gmail.com>
2016-08-30 20:15:39 +0000

    object specs pass

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.apply(obj)
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
+    return constructor;
   },
 
   iterate: function(obj) {
-
+    var resultArr = [];
+    var str = '';
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        str = key + ': ' + obj[key];
+        resultArr.push(str)
+      }
+    }    
+    return resultArr;
   }
 };
