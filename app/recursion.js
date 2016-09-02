bf1c7982471d72d114015705b8503ae372c83255
John Nieves <karn09@gmail.com>
2016-09-01 22:52:45 -0400

    flow control, logic, somme recursion, and some regex specs.

--- app/recursion.js
+++ app/recursion.js
@@ -2,14 +2,29 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    var files = [];
 
+    (function process(dir) {
+        dir.files.forEach(function (file) {
+          if (typeof file === 'string') {
+            files.push(file);
+          } else {
+            return process(file);
+          }
+        })
+    })(data);
+
+    return files;
   },
 
   permute: function(arr) {
+    var permArr = [];
 
+    return permArr;
   },
 
   fibonacci: function(n) {
+
 
   },
 
--- app/regex.js
+++ app/regex.js
@@ -2,23 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return /\d/.test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    return /(\D)(\1)/.test(str)
   },
 
   endsWithVowel: function(str) {
-
+    return /([aA]|[eE]|[iI]|[oO]|[uU])$/.test(str)
   },
 
   captureThreeNumbers: function(str) {
-
+    var found = str.match(/\d{3}/);
+    return found ? found[0] : false;
   },
 
   matchesPattern: function(str) {
-
+    return /^\d{3}-\d{3}-\d{4}$/.test(str)
   },
 
   isUSD: function(str) {
